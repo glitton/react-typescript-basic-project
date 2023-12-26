5d0adc80231daff198870d94046af74d6dcd055e
Generosa Litton <glcodeworks@gmail.com>
2023-12-26 18:37:30 -0500

    polymorphic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 // import Input from "./components/Input.tsx";
 import Button from "./components/Button.tsx";
+import Container from "./components/Container.tsx";
 
 function App() {
   return (
     <main>
       {/* <Input id='name' label='Your name' type='text' />
       <Input id='age' label='Your age' type='number' /> */}
-      <p>
+      {/* <p>
         <Button>A button</Button>
       </p>
       <p>
         <Button href='https://glcodeworks.com'>A link</Button>
-      </p>
+      </p> */}
+      <Container
+        as={Button}
+        onClick={() => console.log("you clicked me")}
+        type='button'
+      >
+        Click Me
+      </Container>
     </main>
   );
 }
